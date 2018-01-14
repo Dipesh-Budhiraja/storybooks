258ac7709021a1f820fad85fcb374da768da5412
Dipesh-Budhiraja <25dipesh98@gmail.com>
2018-01-15 01:46:17 +0530

    handled auth

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//passport config
+require('./config/passport')(passport);
+
+//load routes
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('It Works');
 });
+
+//use routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 3000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://admin:admin@ds251827.mlab.com:51827/storybooks-dev',
+    googleClientID: '459273341956-d4o3bfu8s6qd9etaipkk40fnhf4aj238.apps.googleusercontent.com',
+    googleClientSecret: 'oKxVGmVyf22fnXVNcmzCoKbx'
+}
